3b14d5718852e6869628242397489102e53e52a1
oradkovsky <ostap.radkovsky@gmail.com>
2015-11-21 20:47:55 +0200

    Minor refinement changes needed for lib distribution. Support libraries and tools update.

--- app/src/main/java/com/codecomputerlove/fastscrollrecyclerviewdemo/FastScrollRecyclerView.java
+++ app/src/main/java/com/codecomputerlove/fastscrollrecyclerviewdemo/FastScrollRecyclerView.java
@@ -1,74 +1,74 @@
 package com.codecomputerlove.fastscrollrecyclerviewdemo;
 
 import android.content.Context;
-import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.os.Handler;
-import android.os.Message;
 import android.support.v7.widget.RecyclerView;
 import android.util.AttributeSet;
+import android.util.TypedValue;
 import android.view.MotionEvent;
 
+import com.codecomputerlove.fastscrollrecyclerviewdemo.interfaces.FastScrollRecyclerViewSupport;
+
+import java.lang.ref.WeakReference;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Set;
 
+import lombok.Getter;
+import lombok.experimental.Accessors;
+
 /**
  * Created by flaviusmester on 23/02/15.
  */
+@Accessors(prefix = "m")
 public class FastScrollRecyclerView extends RecyclerView {
-    private Context ctx;
+    private boolean mIsConfigured;
 
-    private boolean setupThings = false;
-    public static int indWidth = 25;
-    public static int indHeight= 18;
-    public float scaledWidth;
-    public float scaledHeight;
-    public String[] sections;
-    public float sx;
-    public float sy;
-    public String section;
-    public boolean showLetter = false;
-    private Handler listHandler;
+    @Getter
+    private String mFsCurrentSection;
+    @Getter
+    private float mFsStartingX;
+    @Getter
+    private float mFsStartingY;
+    @Getter
+    private boolean mFsShouldBeShown;
+    @Getter
+    private String[] mFsSections;
+    @Getter
+    private int mFsItemWidth;
+    @Getter
+    private int mFsItemHeight;
+    @Getter
+    private float mFsItemWidthPx;
+    @Getter
+    private float mFsItemHeightPx;
 
     public FastScrollRecyclerView(Context context) {
         super(context);
-        ctx = context;
+
+        setupDefaults();
     }
 
     public FastScrollRecyclerView(Context context, AttributeSet attrs) {
         super(context, attrs);
-        ctx = context;
+
+        setupDefaults();
     }
 
     public FastScrollRecyclerView(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
-        ctx = context;
+
+        setupDefaults();
     }
 
     @Override
     public void onDraw(Canvas c) {
-        if(!setupThings)
-            setupThings();
-        super.onDraw(c);
-    }
-
-    private void setupThings() {
-        //create az text data
-        Set<String> sectionSet = ((FastScrollRecyclerViewInterface)getAdapter()).getMapIndex().keySet();
-        ArrayList<String> listSection = new ArrayList<>(sectionSet);
-        Collections.sort(listSection);
-        sections = new String[listSection.size()];
-        int i=0;
-        for(String s:listSection) {
-            sections[i++] = s;
+        if (!mIsConfigured) {
+            configure();
         }
 
-        scaledWidth = indWidth * ctx.getResources().getDisplayMetrics().density;
-        scaledHeight= indHeight* ctx.getResources().getDisplayMetrics().density;
-        sx = this.getWidth() - this.getPaddingRight() - (float)(1.2*scaledWidth);
-        sy = (float)((this.getHeight() - (scaledHeight * sections.length) )/2.0);
-        setupThings = true;
+        super.onDraw(c);
     }
 
     @Override
@@ -78,67 +78,120 @@ public class FastScrollRecyclerView extends RecyclerView {
 
         switch (event.getAction()) {
             case MotionEvent.ACTION_DOWN: {
-                if (x < sx - scaledWidth || y < sy || y > (sy + scaledHeight*sections.length))
+                if (isCoordinateOutsideRange(x, y))
                     return super.onTouchEvent(event);
                 else {
-                    // We touched the index bar
-                    float yy = y - this.getPaddingTop() - getPaddingBottom() - sy;
-                    int currentPosition = (int) Math.floor(yy / scaledHeight);
-                    if(currentPosition<0)currentPosition=0;
-                    if(currentPosition>=sections.length)currentPosition=sections.length-1;
-                    section = sections[currentPosition];
-                    showLetter = true;
-                    int positionInData = 0;
-                    if( ((FastScrollRecyclerViewInterface)getAdapter()).getMapIndex().containsKey(section.toUpperCase()) )
-                        positionInData = ((FastScrollRecyclerViewInterface)getAdapter()).getMapIndex().get(section.toUpperCase());
-                    this.scrollToPosition(positionInData);
-                    FastScrollRecyclerView.this.invalidate();
+                    mFsCurrentSection = getFsSections()[getCurrentPosition(y - this.getPaddingTop() - getPaddingBottom() - getFsStartingY())]
+                            .toUpperCase();
+
+                    scrollToPosition();
                 }
+
                 break;
             }
             case MotionEvent.ACTION_MOVE: {
-
-                if (!showLetter && (x < sx  - scaledWidth || y < sy || y > (sy + scaledHeight*sections.length)))
+                if (!mFsShouldBeShown && isCoordinateOutsideRange(x, y))
                     return super.onTouchEvent(event);
                 else {
-                    float yy = y - sy;
-                    int currentPosition = (int) Math.floor(yy / scaledHeight);
-                    if(currentPosition<0)currentPosition=0;
-                    if(currentPosition>=sections.length)currentPosition=sections.length-1;
-                    section = sections[currentPosition];
-                    showLetter = true;
-                    int positionInData = 0;
-                    if(((FastScrollRecyclerViewInterface)getAdapter()).getMapIndex().containsKey(section.toUpperCase()) )
-                        positionInData = ((FastScrollRecyclerViewInterface)getAdapter()).getMapIndex().get(section.toUpperCase());
-                    this.scrollToPosition(positionInData);
-                    FastScrollRecyclerView.this.invalidate();
+                    mFsCurrentSection = getFsSections()[getCurrentPosition(y - getFsStartingY())]
+                            .toUpperCase();
 
+                    scrollToPosition();
                 }
-                break;
 
+                break;
             }
             case MotionEvent.ACTION_UP: {
-                listHandler = new ListHandler();
-                listHandler.sendEmptyMessageDelayed(0, 100);
-                if (x < sx - scaledWidth || y < sy || y > (sy + scaledHeight*sections.length))
-                    return super.onTouchEvent(event);
-                else
-                    return true;
-//                break;
+                new Handler().postDelayed(new NotifierRunnable(this), 100);
+
+
+                return !isCoordinateOutsideRange(x, y) || super.onTouchEvent(event);
             }
         }
+
         return true;
     }
 
-    private class ListHandler extends Handler {
+    private void setupDefaults() {
+        mFsItemWidth = 24;
+        mFsItemHeight = 24;
+        mFsCurrentSection = "";
+    }
 
-        @Override
-        public void handleMessage(Message msg) {
-            super.handleMessage(msg);
-            showLetter = false;
-            FastScrollRecyclerView.this.invalidate();
+    private void configure() {
+        Set<String> sectionSet = ((FastScrollRecyclerViewSupport) getAdapter()).getMapIndex().keySet();
+        ArrayList<String> listSection = new ArrayList<>(sectionSet);
+        Collections.sort(listSection);
+
+        mFsSections = new String[listSection.size()];
+
+        for (int i = 0; i < mFsSections.length; i++) {
+            mFsSections[i] = listSection.get(i).toUpperCase();
         }
 
+        mFsItemWidthPx = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, getFsItemWidth(),
+                getContext().getResources().getDisplayMetrics());
+        mFsItemHeightPx = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, getFsItemHeight(),
+                getContext().getResources().getDisplayMetrics());
 
+        mFsStartingX = this.getWidth() - this.getPaddingRight() - getFsItemWidthPx();
+
+        mFsStartingY = (float) ((this.getHeight() - (getFsItemHeightPx() * getFsSections().length)) / 2.0);
+
+        if (mFsStartingY < 0) {
+            mFsStartingY = 0;
+        }
+
+        mIsConfigured = true;
+    }
+
+    private int getCurrentPosition(float yy) {
+        int currentPosition = (int) Math.floor(yy / getFsItemHeightPx());
+
+        if (currentPosition < 0) {
+            currentPosition = 0;
+        } else if (currentPosition >= getFsSections().length) {
+            currentPosition = getFsSections().length - 1;
+        }
+
+        return currentPosition;
+    }
+
+    private void scrollToPosition() {
+        mFsShouldBeShown = true;
+
+        int positionInData = 0;
+        if (((FastScrollRecyclerViewSupport) getAdapter()).getMapIndex().containsKey(mFsCurrentSection.toUpperCase())) {
+            positionInData = ((FastScrollRecyclerViewSupport) getAdapter()).getMapIndex().get(mFsCurrentSection.toUpperCase());
+        }
+
+        //influenced by https://github.com/appukrb
+        getLayoutManager().scrollToPosition(positionInData);
+
+        invalidate();
+    }
+
+    private boolean isCoordinateOutsideRange(float x, float y) {
+        return x < getFsStartingX() - getFsItemWidthPx() ||
+                y < getFsStartingY() ||
+                y > (getFsStartingY() + getFsItemHeightPx() * getFsSections().length);
+    }
+
+    private static class NotifierRunnable implements Runnable {
+        private WeakReference<FastScrollRecyclerView> rvReference;
+
+        public NotifierRunnable(FastScrollRecyclerView value) {
+            rvReference = new WeakReference<>(value);
+        }
+
+        @Override
+        public void run() {
+            FastScrollRecyclerView rv = rvReference.get();
+
+            if (rv != null) {
+                rv.mFsShouldBeShown = false;
+                rv.invalidate();
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/codecomputerlove/fastscrollrecyclerviewdemo/interfaces/FastScrollRecyclerViewSupport.java
@@ -0,0 +1,10 @@
+package com.codecomputerlove.fastscrollrecyclerviewdemo.interfaces;
+
+import java.util.HashMap;
+
+/**
+ * Created by flaviusmester on 23/02/15.
+ */
+public interface FastScrollRecyclerViewSupport {
+    HashMap<String,Integer> getMapIndex();
+}
